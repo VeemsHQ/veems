4080b0d6fa620a1d381bb1203aa9dad70bd8a965
Richard O'Dwyer <richard@richard.do>
2020-12-02 20:33:42 +0000

    fix upload folder for now, non-s3

--- veems/media/video/transcoder/manager.py
+++ veems/media/video/transcoder/manager.py
@@ -35,11 +35,10 @@ def task_transcode(upload_id, transcode_job_id):
     upload = models.Upload.objects.get(id=upload_id)
     transcode_job = models.TranscodeJob.objects.get(id=transcode_job_id)
     _mark_transcode_job_processing(transcode_job=transcode_job)
-    uploaded_file = tempfile.NamedTemporaryFile(
-        suffix=upload.file.name, delete=False
+    uploaded_file = tempfile.NamedTemporaryFile(suffix=upload.file.name,
     )
     with uploaded_file as file_:
         file_.write(upload.file.read())
-    transcode_executor.transcode(
-        transcode_job=transcode_job, source_file_path=Path(uploaded_file.name)
-    )
+        transcode_executor.transcode(
+            transcode_job=transcode_job, source_file_path=Path(uploaded_file.name)
+        )
