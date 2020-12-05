31602c64e30cb8fda0e932d5ce0b30a137ade9fc
Richard O'Dwyer <richard@richard.do>
2020-12-05 16:09:58 +0000

    bash script to gen thumbs across video

--- tests/test_data/gen.sh
+++ tests/test_data/gen.sh
@@ -1,3 +1,8 @@
 # ffmpeg -i 2160p_24fps.mp4 -vf fps=1 out%d.png
 
-ffmpeg -i 1080p_30fps_vertical.webm -vf "select=gt(scene\,0.4)" -vsync vfr -vf fps=3/60 img%03d.jpg
+plist=(15, 46, 77, 108, 139, 171, 202, 233, 264, 295, 326, 357, 388, 419, 450, 481, 513, 544, 575, 606, 637)
+for i in ${plist[@]}; do
+    start_time=${i//,/}
+    echo $start_time
+    ffmpeg -ss $start_time -i 360p_30fps_10min.mp4 -vf select="eq(pict_type\,I)" -vframes 1 thumbs/$start_time.png
+done
