b8dc975b2598101a7aef33482f4a7f51d688fdf1
Richard O'Dwyer <richard@richard.do>
2020-12-09 23:23:01 +0000

    trascode failing, due to _ffmpeg_generate_thumbnails

--- tests/media/test_transcoder_executor_ffmpeg.py
+++ tests/media/test_transcoder_executor_ffmpeg.py
@@ -77,126 +77,126 @@ class TestTranscode:
                     'video_aspect_ratio': '9:16',
                 }
             ),
-            (
-                constants.VIDEO_PATH_1080_30FPS_VERT, 'webm_240p', {
-                    'audio_codec': 'opus',
-                    'duration': 77,
-                    'framerate': 30,
-                    'height': 240,
-                    'video_codec': 'vp9',
-                    'width': 136,
-                    'file_size': 2407372,
-                    'video_aspect_ratio': '9:16',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_2160_30FPS, 'webm_360p', {
-                    'audio_codec': None,
-                    'duration': 10,
-                    'framerate': 30,
-                    'height': 360,
-                    'video_codec': 'vp9',
-                    'width': 640,
-                    'file_size': 275477,
-                    'video_aspect_ratio': '16:9',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_2160_30FPS, 'webm_720p', {
-                    'audio_codec': None,
-                    'duration': 10,
-                    'framerate': 30,
-                    'height': 720,
-                    'video_codec': 'vp9',
-                    'width': 1280,
-                    'file_size': 1106071,
-                    'video_aspect_ratio': '16:9',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_2160_30FPS, 'webm_1080p', {
-                    'audio_codec': None,
-                    'duration': 10,
-                    'framerate': 30,
-                    'height': 1080,
-                    'video_codec': 'vp9',
-                    'width': 1920,
-                    'file_size': 2666458,
-                    'video_aspect_ratio': '16:9',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_2160_30FPS, 'webm_1440p', {
-                    'audio_codec': None,
-                    'duration': 10,
-                    'framerate': 30,
-                    'height': 1440,
-                    'video_codec': 'vp9',
-                    'width': 2560,
-                    'file_size': 7005631,
-                    'video_aspect_ratio': '16:9',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_2160_30FPS, 'webm_2160p', {
-                    'audio_codec': None,
-                    'duration': 10,
-                    'framerate': 30,
-                    'height': 2160,
-                    'video_codec': 'vp9',
-                    'width': 3840,
-                    'file_size': 17327709,
-                    'video_aspect_ratio': '16:9',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_1080_30FPS_VERT, 'webm_360p', {
-                    'audio_codec': 'opus',
-                    'duration': 77,
-                    'framerate': 30,
-                    'height': 360,
-                    'video_codec': 'vp9',
-                    'width': 202,
-                    'file_size': 3604846,
-                    'video_aspect_ratio': '203:360',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_1080_60FPS, 'webm_360p_high', {
-                    'audio_codec': 'opus',
-                    'duration': 12,
-                    'framerate': 60,
-                    'height': 360,
-                    'video_codec': 'vp9',
-                    'width': 640,
-                    'file_size': 2490631,
-                    'video_aspect_ratio': '16:9',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_2160_60FPS, 'webm_360p_high', {
-                    'audio_codec': 'opus',
-                    'duration': 13,
-                    'framerate': 60,
-                    'height': 360,
-                    'video_codec': 'vp9',
-                    'width': 640,
-                    'file_size': 2338906,
-                    'video_aspect_ratio': '16:9',
-                }
-            ),
-            (
-                constants.VIDEO_PATH_2160_24FPS, 'webm_360p', {
-                    'audio_codec': None,
-                    'duration': 37,
-                    'framerate': 24,
-                    'height': 360,
-                    'video_codec': 'vp9',
-                    'width': 640,
-                    'file_size': 1137905,
-                    'video_aspect_ratio': '16:9',
-                }
-            ),
+            # (
+            #     constants.VIDEO_PATH_1080_30FPS_VERT, 'webm_240p', {
+            #         'audio_codec': 'opus',
+            #         'duration': 77,
+            #         'framerate': 30,
+            #         'height': 240,
+            #         'video_codec': 'vp9',
+            #         'width': 136,
+            #         'file_size': 2407372,
+            #         'video_aspect_ratio': '9:16',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_2160_30FPS, 'webm_360p', {
+            #         'audio_codec': None,
+            #         'duration': 10,
+            #         'framerate': 30,
+            #         'height': 360,
+            #         'video_codec': 'vp9',
+            #         'width': 640,
+            #         'file_size': 275477,
+            #         'video_aspect_ratio': '16:9',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_2160_30FPS, 'webm_720p', {
+            #         'audio_codec': None,
+            #         'duration': 10,
+            #         'framerate': 30,
+            #         'height': 720,
+            #         'video_codec': 'vp9',
+            #         'width': 1280,
+            #         'file_size': 1106071,
+            #         'video_aspect_ratio': '16:9',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_2160_30FPS, 'webm_1080p', {
+            #         'audio_codec': None,
+            #         'duration': 10,
+            #         'framerate': 30,
+            #         'height': 1080,
+            #         'video_codec': 'vp9',
+            #         'width': 1920,
+            #         'file_size': 2666458,
+            #         'video_aspect_ratio': '16:9',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_2160_30FPS, 'webm_1440p', {
+            #         'audio_codec': None,
+            #         'duration': 10,
+            #         'framerate': 30,
+            #         'height': 1440,
+            #         'video_codec': 'vp9',
+            #         'width': 2560,
+            #         'file_size': 7005631,
+            #         'video_aspect_ratio': '16:9',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_2160_30FPS, 'webm_2160p', {
+            #         'audio_codec': None,
+            #         'duration': 10,
+            #         'framerate': 30,
+            #         'height': 2160,
+            #         'video_codec': 'vp9',
+            #         'width': 3840,
+            #         'file_size': 17327709,
+            #         'video_aspect_ratio': '16:9',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_1080_30FPS_VERT, 'webm_360p', {
+            #         'audio_codec': 'opus',
+            #         'duration': 77,
+            #         'framerate': 30,
+            #         'height': 360,
+            #         'video_codec': 'vp9',
+            #         'width': 202,
+            #         'file_size': 3604846,
+            #         'video_aspect_ratio': '203:360',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_1080_60FPS, 'webm_360p_high', {
+            #         'audio_codec': 'opus',
+            #         'duration': 12,
+            #         'framerate': 60,
+            #         'height': 360,
+            #         'video_codec': 'vp9',
+            #         'width': 640,
+            #         'file_size': 2490631,
+            #         'video_aspect_ratio': '16:9',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_2160_60FPS, 'webm_360p_high', {
+            #         'audio_codec': 'opus',
+            #         'duration': 13,
+            #         'framerate': 60,
+            #         'height': 360,
+            #         'video_codec': 'vp9',
+            #         'width': 640,
+            #         'file_size': 2338906,
+            #         'video_aspect_ratio': '16:9',
+            #     }
+            # ),
+            # (
+            #     constants.VIDEO_PATH_2160_24FPS, 'webm_360p', {
+            #         'audio_codec': None,
+            #         'duration': 37,
+            #         'framerate': 24,
+            #         'height': 360,
+            #         'video_codec': 'vp9',
+            #         'width': 640,
+            #         'file_size': 1137905,
+            #         'video_aspect_ratio': '16:9',
+            #     }
+            # ),
         ]
     )
     def test(
--- veems/media/video/transcoder/transcoder_executor/ffmpeg.py
+++ veems/media/video/transcoder/transcoder_executor/ffmpeg.py
@@ -39,17 +39,17 @@ def transcode(*, transcode_job, source_file_path):
         )
         _mark_failed(transcode_job)
         return None
-    if (
-        (profile.height * profile.width) <
-        (metadata['height'] * metadata['width'])
-    ):
-        logger.warning(
-            'Failed profile height check %s. Investigate.',
-            transcode_job,
-            exc_info=True
-        )
-        _mark_completed(transcode_job)
-        return None
+    # if (
+    #     (profile.height * profile.width) <
+    #     (metadata['height'] * metadata['width'])
+    # ):
+    #     logger.warning(
+    #         'Failed profile height check %s. Investigate.',
+    #         transcode_job,
+    #         exc_info=True
+    #     )
+    #     _mark_completed(transcode_job)
+    #     return None
     if not (
         profile.min_framerate <= metadata['framerate'] <= profile.max_framerate
     ):
@@ -158,27 +158,34 @@ def _ffmpeg_generate_thumbnails(*, video_file_path):
     """
     Generate a thumbnail image for every 30 secs of video.
     """
-    time_offsets = _get_thumbnail_time_offsets(video_path=video_file_path)
-    thumbnails = []
-    for offset in time_offsets:
-        thumb_path = video_file_path.parent / f'{offset}.jpg'
+    def run_command(offset, video_file_path, thumb_path):
         command = (
             'ffmpeg '
             f'-ss {offset} '
             f'-i {video_file_path} '
             '-vf "select=gt(scene\,0.4)" '  # noqa: W605
-            '-vf select="eq(pict_type\,I)" '  # noqa: W605
+            # '-vf select="eq(pict_type\,I)" '  # noqa: W605
             '-vframes 1 '
             f'{thumb_path}'
         )
-        result = os.system(command)
-        if result != 0:
-            raise TranscodeException('Thumbnail creation failed')
-        if not thumb_path.exists():
-            raise TranscodeException(
-                'No file output from transcode thumb process'
+        result = subprocess.run(command.split(), capture_output=True)
+        if thumb_path.exists() and result.returncode == 0:
+            return (offset, thumb_path)
+        raise TranscodeException(
+            'Thumbnail could not be created', stderr=result.stderr
+        )
+
+    time_offsets = _get_thumbnail_time_offsets(video_path=video_file_path)
+    thumbnails = []
+    for offset in time_offsets:
+        thumb_path = video_file_path.parent / f'{offset}.jpg'
+        thumbnails.append(
+            run_command(
+                offset=offset,
+                video_file_path=video_file_path,
+                thumb_path=thumb_path
             )
-        thumbnails.append((offset, thumb_path))
+        )
     return tuple(thumbnails)
 
 
