8a21f6585da7f3e30c49ef08ba098aaf77201530
Richard O'Dwyer <richard@richard.do>
2020-12-29 11:48:41 +0000

    rm ip log

--- veems/common/exception_handler.py
+++ veems/common/exception_handler.py
@@ -8,24 +8,10 @@ from django.core.exceptions import ObjectDoesNotExist
 logger = logging.getLogger(__name__)
 
 
-def _get_client_ip(request):
-    if request is None:
-        return None
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
-
-
 def api_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     if isinstance(exc, ObjectDoesNotExist):
-        logger.info(
-            'Not found, client ip: %s', _get_client_ip(context.get('request'))
-        )
         return Response({'detail': 'Not found.'}, status=NOT_FOUND)
 
     return response
