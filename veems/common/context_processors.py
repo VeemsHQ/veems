6027d2231391a18d4e20e4002c44d319ecd96748
Richard O'Dwyer <richard@richard.do>
2021-01-02 16:48:18 +0000

    global_context, not authed fix

--- veems/common/context_processors.py
+++ veems/common/context_processors.py
@@ -5,9 +5,12 @@ from ..user import forms as user_forms
 def global_context(request):
     """Context which is applied to all views."""
     context = {}
-    context['channels'] = channel_services.get_channels(
-        user_id=request.user.id
-    )
+    if request.user.is_authenticated:
+        context['channels'] = channel_services.get_channels(
+            user_id=request.user.id
+        )
+    else:
+        context['channels'] = []
     context['login_form'] = user_forms.CustomAuthenticationForm(request)
     context['next'] = request.GET.get('next') or ''
     return context
