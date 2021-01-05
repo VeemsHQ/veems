67c0dba28947643b58b3107b7f196797dc0fa1ba
Richard O'Dwyer <richard@richard.do>
2021-01-05 21:54:38 +0000

    add image fields to channel model

--- veems/channel/models.py
+++ veems/channel/models.py
@@ -3,6 +3,19 @@ from django.contrib.auth import get_user_model
 
 from ..common.models import BaseModel
 from ..common import validators
+from ..media import storage_backends
+
+STORAGE_BACKEND = storage_backends.MediaStorage
+
+
+def _channel_avatar_image_upload_to(instance, filename):
+    channel = instance
+    return f'channels/profile_images/{channel.id}{Path(filename).suffix}'
+
+
+def _channel_banner_image_upload_to(instance, filename):
+    channel = instance
+    return f'channels/banner_images/{channel.id}{Path(filename).suffix}'
 
 
 class Channel(BaseModel):
@@ -15,6 +28,16 @@ class Channel(BaseModel):
     language = models.CharField(
         max_length=2, validators=(validators.validate_language,), default=None
     )
+    avatar_image = models.ImageField(
+        upload_to=_channel_avatar_image_upload_to, storage=STORAGE_BACKEND,
+        null=True,
+    )
+    banner_image = models.ImageField(
+        upload_to=_channel_banner_image_upload_to, storage=STORAGE_BACKEND,
+        null=True,
+    )
+
+    # TODO: add default images
 
     def __str__(self):
         return f'<{self.__class__.__name__} {self.id} {self.name}>'
