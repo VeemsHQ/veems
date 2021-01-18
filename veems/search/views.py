7851a2a94e1b24e443e18193932c93771eef9f5e
Richard O'Dwyer <richard@richard.do>
2021-01-18 21:26:14 +0000

    styling, more narrow container

--- veems/search/views.py
+++ veems/search/views.py
@@ -4,6 +4,9 @@ from ..media import (
     serializers as media_serializers,
     services as media_services,
 )
+from ..channel import (
+    services as channel_services, serializers as channel_serializers
+)
 
 
 class SearchView(TemplateView):
@@ -11,11 +14,20 @@ class SearchView(TemplateView):
 
     def get_context_data(self, *args, query, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        videos = media_services.get_popular_videos()
-        videos = media_serializers.VideoSlimSerializer(
-            videos, many=True
-        ).data
-        context['video_results'] = videos
+        query_type = self.request.GET.get('type', '').lower()
+        context['video_results'] = []
         context['channel_results'] = []
+        if not query_type or query_type == 'videos':
+            videos = media_services.get_popular_videos()
+            videos = media_serializers.VideoSlimSerializer(
+                videos, many=True
+            ).data
+            context['video_results'] = videos
+        elif query_type == 'channels':
+            channels = channel_services.get_channels()
+            channels = channel_serializers.ChannelSlimSerializer(
+                channels, many=True
+            ).data
+            context['channel_results'] = channels
         context['search_query'] = query
         return context
