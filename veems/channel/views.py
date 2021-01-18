177976cd8c429b3ca7d480b784a64eb7f8368713
Richard O'Dwyer <richard@richard.do>
2021-01-18 14:21:24 +0000

    context mixin

--- veems/channel/views.py
+++ veems/channel/views.py
@@ -7,38 +7,34 @@ from ..media import (
 from . import services, serializers
 
 
-def _get_context_data(*, context, channel_id, request, include_videos=False):
-    if include_videos:
-        videos = media_services.get_videos(channel_id=channel_id)
-        videos_data = media_serializers.VideoSlimSerializer(
-            videos, many=True
-        ).data
-        context['channel_videos'] = videos_data
-    channel = services.get_channel(id=channel_id)
-    channel_data = serializers.ChannelSlimSerializer(channel).data
-    context['channel'] = channel_data
-    context['is_owner'] = channel_data['user'] == request.user.id
-    return context
+class ChannelContextMixin:
+    def get_context_data(
+        self, *args, channel_id, include_videos=False, **kwargs
+    ):
+        context = super().get_context_data(
+            *args, channel_id=channel_id, **kwargs
+        )
+        if include_videos:
+            videos = media_services.get_videos(channel_id=channel_id)
+            videos_data = media_serializers.VideoSlimSerializer(
+                videos, many=True
+            ).data
+            context['channel_videos'] = videos_data
+        channel = services.get_channel(id=channel_id)
+        channel_data = serializers.ChannelSlimSerializer(channel).data
+        context['channel'] = channel_data
+        context['is_owner'] = channel_data['user'] == self.request.user.id
+        return context
 
 
-class ChannelIndexView(TemplateView):
+class ChannelIndexView(ChannelContextMixin, TemplateView):
     template_name = 'channel/index.html'
 
     def get_context_data(self, *args, channel_id, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        return _get_context_data(
-            context=context,
-            channel_id=channel_id,
-            request=self.request,
-            include_videos=True,
+        return super().get_context_data(
+            *args, channel_id=channel_id, include_videos=True, **kwargs
         )
 
 
-class ChannelAboutView(TemplateView):
+class ChannelAboutView(ChannelContextMixin, TemplateView):
     template_name = 'channel/about.html'
-
-    def get_context_data(self, *args, channel_id, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        return _get_context_data(
-            context=context, channel_id=channel_id, request=self.request
-        )
