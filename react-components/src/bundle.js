f4656afbcf192ead6adfc11cfa70278b19ffc510
Richard O'Dwyer <richard@richard.do>
2021-01-21 22:40:47 +0000

    rm unused home component

--- react-components/src/bundle.js
+++ react-components/src/bundle.js
@@ -1,10 +1,8 @@
-import { createHello } from '@src/components/Hello/hello';
 import { CreateChannel } from '@src/components/CreateChannel';
 import { SyncChannel } from '@src/components/SyncChannel';
 import { SelectChannel } from '@src/components/SelectChannel';
 
 export default {
-    createHello,
     CreateChannel,
     SyncChannel,
     SelectChannel,
