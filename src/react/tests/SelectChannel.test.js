33d6b1451245b2a055a7f35b9fbe3ef351a6691c
GoldAndYouth <davidhughes13@gmail.com>
2021-01-26 18:03:47 +0000

    Add basic tests for select channel

---
+++ src/react/tests/SelectChannel.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+
+import Adapter from 'enzyme-adapter-react-16'
+
+import { SelectChannelContainer } from '../../../react-components/src/components/SelectChannel';
+
+const setUp = (props = {}) => {
+  const component = Enzyme.shallow(<SelectChannelContainer {...props} />);
+  return component;
+}
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('SelectChannel-Component', () => {
+  let component;
+
+  beforeEach(() => {
+    const props = {};
+    component = setUp(props);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Should render without errors', () => {
+    expect(component.exists()).toBe(true);
+  });
+});
