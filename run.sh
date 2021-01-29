3f81bd0843a404cc2829655630d52dc8fee79734
Richard O'Dwyer <richard@richard.do>
2021-01-29 10:27:26 +0000

    Build JS in Dockerfile

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-
 set -e
-
-# python manage.py compress
-echo "Collecting static files"
-python manage.py collectstatic --noinput
 echo "Running migrations"
 python manage.py migrate --noinput
+echo "Collecting static files"
+python manage.py collectstatic --noinput
 echo "Running web service"
 uwsgi --ini uwsgi.ini
