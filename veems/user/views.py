4a26363af7ac6750adeb1df71e912fa632dc199d
Richard O'Dwyer <richard@richard.do>
2021-02-12 19:53:04 +0000

    login redirect

--- veems/user/views.py
+++ veems/user/views.py
@@ -17,6 +17,11 @@ logger = logging.getLogger(__name__)
 class CustomLoginView(views.LoginView):
     form_class = forms.CustomAuthenticationForm
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['login_form'] = context.pop('form')
