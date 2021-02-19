5cc299cfc61610cea78fe176d5f517925c6c1a0e
Richard O'Dwyer <richard@richard.do>
2021-02-19 18:42:24 +0000

    file drag and drop

--- react-components/src/components/VideoDetailModal/FileUploadChooseModal.js
+++ react-components/src/components/VideoDetailModal/FileUploadChooseModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
@@ -10,34 +11,42 @@ export const FileUploadChooseModal = ({
     onModalClose = null,
     onModalOpen = null,
 }) => {
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+    const files = acceptedFiles.map(file => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes
+        </li>
+    ));
+
+    const handleChange = (e) => {
+        console.log(e);
+    }
 
     const renderModal = () => (
         <>
             {isModalOpen && (
                 <Modal show={isModalOpen}>
-                    <Form>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Upload videos</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <div class="d-flex flex-column align-items-center">
-                                <div class="my-3 d-inline-flex align-self-center align-items-center justify-content-center bg-light rounded-circle"
+                    <Modal.Header closeButton>
+                        <Modal.Title>Upload videos</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div {...getRootProps({ className: 'dropzone d-flex flex-column align-items-center' })}>
+                            <div className="d-flex flex-column align-items-center">
+                                <div className="mt-3 mb-2 d-inline-flex align-self-center align-items-center justify-content-center bg-light rounded-circle"
                                     style={{ width: '150px', height: '150px' }}>
-                                    <i class="material-icons  text-secondary" style={{ fontSize: '72px' }}>publish</i>
+                                    <i className="material-icons  text-secondary" style={{ fontSize: '72px' }}>publish</i>
                                 </div>
-                                <Form className="mt-3 col-md-8 col-lg-6">
-                                    <Form.File
-                                        id="file"
-                                        label="Choose video file to upload"
-                                        custom
-                                    />
-                                </Form>
-                                <p class="mt-3 align-self-center">Your videos will be private until you
-                        publish them.</p>
+                                <input {...getInputProps()} />
+                                <p>Drag and drop video files here to upload</p>
+                                <p className="text-muted align-self-center">Your videos will be private until you publish them.</p>
+                                <p><button type="button" class="btn btn-primary">Select Files</button></p>
                             </div>
-                        </Modal.Body>
-
-                    </Form>
+                            <aside className="d2-none">
+                                <h4>Files</h4>
+                                <ul>{files}</ul>
+                            </aside>
+                        </div>
+                    </Modal.Body>
                 </Modal>
             )}
         </>
